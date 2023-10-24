efee845c94f965318697eec4d92724014193b1a9
JohnnyFDev <johnnyfreirejh@hotmail.com>
2023-10-24 09:31:25 -0300

    use theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:minicurso_marvel_api/shared/theme/custom_theme.dart';
 
 import 'app/models/marvel_character_model.dart';
 import 'app/presenter/character_detail_page.dart';
@@ -15,9 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Marvel API  - Minicurso',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-      ),
+      theme: CustomTheme.darkTheme,
       initialRoute: '/',
       routes: {
         '/': (context) => const CharacterListPage(),
